7f317ec5705db9f1fa60a00c0da87dabd6465967
tregard <teddy.developper@gmail.com>
2018-12-18 16:25:34 +0100

    all

---
+++ php-src/api/src/Auth.php
@@ -0,0 +1,12 @@
+<?php
+namespace Dealcar\Api;
+
+class Auth{
+	
+	protected $_version = "apiv1";
+	
+	public function __construct()
+	{
+		
+	}
+}
---
+++ php-src/lib/src/Auth.php
@@ -0,0 +1,12 @@
+<?php
+namespace DealCar\Lib;
+
+class Auth
+{
+	protected $_appVersion = "1.0.0";
+	protected $_appType = "lib";
+	public function __construct()
+	{
+		
+	}
+}
